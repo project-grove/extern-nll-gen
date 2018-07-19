8a23b08dd9612477c483ea52c4c0d2eadf69b7eb
Adel Vilkov <vilkov.adel@gmail.com>
2018-07-19 20:09:57 +0300

    WIP Signature generator, missing test

--- src/Processor.cs
+++ src/Processor.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
 
 namespace extern_nll_gen
 {
@@ -13,16 +14,24 @@ namespace extern_nll_gen
         public static string Process(string source)
         {
             var nodes = Parse(source);
-            var externMethods = GetExternMethods(nodes);
+            // Step 1 - add necessary usings
+            nodes = nodes.AddUsings(SyntaxFactory.UsingDirective(
+                SyntaxFactory.ParseName("NativeLibraryLoader")));
+
+            // Step 2 - get all extern methods and remove them from source
+            var externMethods = GetExternMethods(nodes.DescendantNodes());
+            foreach (var externMethod in externMethods)
+                nodes = nodes.RemoveNode(externMethod, SyntaxRemoveOptions.KeepNoTrivia);
+
             // TODO
-            return "";
+
+            return Formatter.Format(nodes, new AdhocWorkspace()).ToFullString();
         }
 
-        internal static IEnumerable<SyntaxNode> Parse(string source) =>
+        internal static CompilationUnitSyntax Parse(string source) =>
             (CSharpSyntaxTree
                 .ParseText(source)
-                .GetRoot() as CompilationUnitSyntax)
-                .DescendantNodesAndSelf();
+                .GetRoot() as CompilationUnitSyntax);
 
         internal static IEnumerable<MethodDeclarationSyntax> GetExternMethods(
             IEnumerable<SyntaxNode> nodes
@@ -31,7 +40,69 @@ namespace extern_nll_gen
                 .Select(node => (MethodDeclarationSyntax)node)
                 .Where(method => method.Modifiers
                     .Select(modifier => modifier.Text)
-                    .Contains("extern"))
-                .ToList();
+                    .Contains("extern"));
+
+        // TODO Check
+        internal static IEnumerable<SyntaxNode> CreateMethodPointer(MethodDeclarationSyntax source)
+        {
+            var methodName = source.Identifier.Text; // TODO Read from DllImport if specified
+            var delegateTypeName = source.Identifier.Text + "_t";
+            var fieldName = "s_" + delegateTypeName;
+
+            var @delegate = SyntaxFactory.DelegateDeclaration(
+                Empty<AttributeListSyntax>(),
+                new SyntaxTokenList(Token(SyntaxKind.PrivateKeyword)),
+                source.ReturnType,
+                Identifier(delegateTypeName),
+                source.TypeParameterList,
+                source.ParameterList,
+                source.ConstraintClauses);
+
+            var @field = SyntaxFactory.ParseStatement(
+                $"private static {delegateTypeName} {fieldName} = " +
+                $"LoadFunction<{delegateTypeName}>(\"{methodName}\");"
+                );
+
+            var @method = SyntaxFactory.MethodDeclaration(
+                Empty<AttributeListSyntax>(),
+                new SyntaxTokenList(
+                    Token(SyntaxKind.PublicKeyword),
+                    Token(SyntaxKind.StaticKeyword)
+                ),
+                @delegate.ReturnType,
+                null,
+                Identifier(methodName),
+                @delegate.TypeParameterList,
+                @delegate.ParameterList,
+                @delegate.ConstraintClauses,
+                null,
+                MethodPointer(Identifier(fieldName), @delegate.ParameterList)
+            );
+            return new List<SyntaxNode>
+            {
+                @delegate, field, method
+            };
+        }
+
+        internal static SyntaxToken Token(SyntaxKind kind) =>
+            SyntaxFactory.Token(kind);
+
+        internal static SyntaxList<T> Empty<T>() where T : SyntaxNode =>
+            new SyntaxList<T>();
+
+        internal static SyntaxToken Identifier(string name) =>
+            SyntaxFactory.Identifier(name);
+
+        internal static TypeSyntax OfType(string typeName) =>
+            SyntaxFactory.IdentifierName(Identifier(typeName));
+
+        internal static ArrowExpressionClauseSyntax MethodPointer(
+            SyntaxToken identifier,
+            ParameterListSyntax parameters
+            )
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
     }
 }
--- test/ProcessorTest.cs
+++ test/ProcessorTest.cs
@@ -1,4 +1,7 @@
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 using static extern_nll_gen.Processor;
 
@@ -6,6 +9,27 @@ namespace test
 {
     public class ProcessorTest
     {
+        [Fact]
+        public void ShouldOutputCode()
+        {
+            var source = "public class MyClass { }";
+            var result = Process(source);
+            Assert.Contains(source, result);
+        }
+
+        [Fact]
+        public void ShouldAddUsings()
+        {
+            var source = Process("");
+            var expected = new List<string> { "NativeLibraryLoader" };
+            var actual = Parse(source).DescendantNodes()
+                .Where(node => node is UsingDirectiveSyntax)
+                .Select(node => (UsingDirectiveSyntax)node)
+                .Select(u => u.Name.ToString());
+
+            Assert.Equal(expected, actual);
+        }
+
         [Fact]
         public void ShouldFindExternMethods()
         {
@@ -17,8 +41,26 @@ namespace test
                     
                     public static void OtherMethod() {}
                 }";
-            var methods = GetExternMethods(Parse(source));
+            var methods = GetExternMethods(Parse(source).DescendantNodes());
             Assert.Single(methods);
+        }
+
+        [Fact]
+        public void ShouldRemoveExternMethods()
+        {
+            var source = @"
+                public static class NativeClass
+                {
+                    [DllImport]
+                    public static extern void myNativeMethod(int param, double[] values);
+                    
+                    public static void OtherMethod() {}
+                }";
+            var processedSource = Process(source);
+            var methodsBefore = GetExternMethods(Parse(source).DescendantNodes());
+            var methodsAfter = GetExternMethods(Parse(processedSource).DescendantNodes());
+            Assert.Single(methodsBefore);
+            Assert.Empty(methodsAfter);
         }
     }
 }
