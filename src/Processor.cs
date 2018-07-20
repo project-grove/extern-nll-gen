dc6268eebeeef85cd00a7bb59f54a6d2d3ddced0
Adel Vilkov <vilkov.adel@gmail.com>
2018-07-20 12:30:13 +0300

    (almost) complete implementation with tests

--- src/Processor.cs
+++ src/Processor.cs
@@ -11,6 +11,8 @@ namespace extern_nll_gen
 {
     public static class Processor
     {
+        internal const string LoadFunctionName = "__LoadFunction";
+
         public static string Process(string source)
         {
             var nodes = Parse(source);
@@ -18,13 +20,33 @@ namespace extern_nll_gen
             nodes = nodes.AddUsings(SyntaxFactory.UsingDirective(
                 SyntaxFactory.ParseName("NativeLibraryLoader")));
 
-            // Step 2 - get all extern methods and remove them from source
+            // Step 2 - get all extern methods and replace them
             var externMethods = GetExternMethods(nodes.DescendantNodes());
+            var classNames = new HashSet<string>();
             foreach (var externMethod in externMethods)
-                nodes = nodes.RemoveNode(externMethod, SyntaxRemoveOptions.KeepNoTrivia);
+            {
+                var parent = externMethod.Parent as ClassDeclarationSyntax;
+                classNames.Add(parent.Identifier.Text);
+                nodes = nodes.RemoveNode(parent, SyntaxRemoveOptions.KeepNoTrivia);
+                parent = parent.RemoveNode(externMethod, SyntaxRemoveOptions.KeepNoTrivia);
+                foreach (var member in CreateMethodPointer(externMethod))
+                    parent = parent.AddMembers(member);
+                nodes = nodes.AddMembers(parent);
+            }
 
-            // TODO
+            // Step 3 - generate loader function stub in each parent class
+            var parentClasses = nodes.DescendantNodes()
+                .Where(node => node is ClassDeclarationSyntax)
+                .Select(node => (ClassDeclarationSyntax)node)
+                .Where(c => classNames.Contains(c.Identifier.Text));
+            foreach (var parent in parentClasses)
+            {
+                nodes = nodes.RemoveNode(parent, SyntaxRemoveOptions.KeepNoTrivia);
+                var newParent = parent.AddMembers(LoaderFunctionStub());
+                nodes = nodes.AddMembers(newParent);
+            }
 
+            // Step 4 - get the modified source
             return Formatter.Format(nodes, new AdhocWorkspace()).ToFullString();
         }
 
@@ -43,12 +65,13 @@ namespace extern_nll_gen
                     .Contains("extern"));
 
         // TODO Check
-        internal static IEnumerable<SyntaxNode> CreateMethodPointer(MethodDeclarationSyntax source)
+        internal static IEnumerable<MemberDeclarationSyntax> CreateMethodPointer(MethodDeclarationSyntax source)
         {
             var methodName = source.Identifier.Text; // TODO Read from DllImport if specified
             var delegateTypeName = source.Identifier.Text + "_t";
             var fieldName = "s_" + delegateTypeName;
 
+            // private delegate T MethodName_t(...);
             var @delegate = SyntaxFactory.DelegateDeclaration(
                 Empty<AttributeListSyntax>(),
                 new SyntaxTokenList(Token(SyntaxKind.PrivateKeyword)),
@@ -58,11 +81,32 @@ namespace extern_nll_gen
                 source.ParameterList,
                 source.ConstraintClauses);
 
-            var @field = SyntaxFactory.ParseStatement(
-                $"private static {delegateTypeName} {fieldName} = " +
-                $"LoadFunction<{delegateTypeName}>(\"{methodName}\");"
+            // private static MethodName_t s_MethodName_t = LoadFunction<T>("MethodName");
+            var fieldVariableInitializer = SyntaxFactory.EqualsValueClause(
+                SyntaxFactory.InvocationExpression(
+                    SyntaxFactory.GenericName(
+                        Identifier(LoadFunctionName),
+                        TypeArgument(delegateTypeName)),
+                    Argument(methodName)));
+            var fieldVariableDeclarator = SyntaxFactory.VariableDeclarator(
+                Identifier(fieldName),
+                null,
+                fieldVariableInitializer
                 );
+            var fieldVariable = SyntaxFactory.VariableDeclaration(
+                OfType(delegateTypeName),
+                Separated(fieldVariableDeclarator)
+            );
+            var @field = SyntaxFactory.FieldDeclaration(
+                Empty<AttributeListSyntax>(),
+                new SyntaxTokenList(
+                    Token(SyntaxKind.PrivateKeyword),
+                    Token(SyntaxKind.StaticKeyword)
+                    ),
+                fieldVariable
+            );
 
+            // public static T MethodName(...) => s_MethodName_t(...);
             var @method = SyntaxFactory.MethodDeclaration(
                 Empty<AttributeListSyntax>(),
                 new SyntaxTokenList(
@@ -76,13 +120,31 @@ namespace extern_nll_gen
                 @delegate.ParameterList,
                 @delegate.ConstraintClauses,
                 null,
-                MethodPointer(Identifier(fieldName), @delegate.ParameterList)
+                MethodPointer(fieldName, @delegate.ParameterList)
             );
-            return new List<SyntaxNode>
+            method = method.WithSemicolonToken(Token(SyntaxKind.SemicolonToken));
+            return new List<MemberDeclarationSyntax>
             {
                 @delegate, field, method
             };
         }
+
+        internal static MethodDeclarationSyntax LoaderFunctionStub() =>
+            SyntaxFactory.MethodDeclaration(
+                Empty<AttributeListSyntax>(),
+                new SyntaxTokenList(
+                    Token(SyntaxKind.PrivateKeyword),
+                    Token(SyntaxKind.StaticKeyword)
+                ),
+                OfType("T"),
+                null,
+                Identifier(LoadFunctionName),
+                TypeParam("T"),
+                Parameter("string", "name"),
+                Empty<TypeParameterConstraintClauseSyntax>(),
+                null,
+                SyntaxFactory.ArrowExpressionClause(
+                    SyntaxFactory.ParseExpression("throw new NotImplementedException();")));
 
         internal static SyntaxToken Token(SyntaxKind kind) =>
             SyntaxFactory.Token(kind);
@@ -90,19 +152,74 @@ namespace extern_nll_gen
         internal static SyntaxList<T> Empty<T>() where T : SyntaxNode =>
             new SyntaxList<T>();
 
+        internal static SeparatedSyntaxList<T> Separated<T>(T node) where T : SyntaxNode =>
+            SyntaxFactory.SeparatedList<T>(new T[] { node });
+
         internal static SyntaxToken Identifier(string name) =>
             SyntaxFactory.Identifier(name);
 
         internal static TypeSyntax OfType(string typeName) =>
             SyntaxFactory.IdentifierName(Identifier(typeName));
 
-        internal static ArrowExpressionClauseSyntax MethodPointer(
-            SyntaxToken identifier,
-            ParameterListSyntax parameters
-            )
+        internal static TypeArgumentListSyntax TypeArgument(string type) =>
+            SyntaxFactory.TypeArgumentList(
+                Token(SyntaxKind.LessThanToken),
+                Separated<TypeSyntax>(OfType(type)),
+                Token(SyntaxKind.GreaterThanToken));
+
+        internal static TypeParameterListSyntax TypeParam(string type) =>
+            SyntaxFactory.TypeParameterList(
+                Token(SyntaxKind.LessThanToken),
+                Separated<TypeParameterSyntax>(
+                    SyntaxFactory.TypeParameter(type)
+                    ),
+                Token(SyntaxKind.GreaterThanToken));
+
+        internal static ArgumentListSyntax Argument(string value) =>
+            SyntaxFactory.ArgumentList(
+                Token(SyntaxKind.OpenParenToken),
+                Separated<ArgumentSyntax>(
+                    SyntaxFactory.Argument(
+                        SyntaxFactory.LiteralExpression(
+                            SyntaxKind.StringLiteralExpression,
+                            SyntaxFactory.Literal(value)))),
+                Token(SyntaxKind.CloseParenToken));
+
+        internal static ParameterListSyntax Parameter(string type, string name) =>
+            SyntaxFactory.ParameterList(
+                Token(SyntaxKind.OpenParenToken),
+                Separated<ParameterSyntax>(
+                    SyntaxFactory.Parameter(
+                        Empty<AttributeListSyntax>(),
+                        new SyntaxTokenList(),
+                        OfType(type),
+                        Identifier(name),
+                        null)),
+                Token(SyntaxKind.CloseParenToken));
+
+        internal static ArgumentListSyntax FromParams(ParameterListSyntax parameters)
         {
-            // TODO
-            throw new NotImplementedException();
+            var nodes = parameters.ChildNodes();
+            var names = parameters.ChildNodes()
+                .Select(n => n as ParameterSyntax)
+                .Select(p => p.Identifier.Text);
+            var arguments = SyntaxFactory.SeparatedList<ArgumentSyntax>(
+                    names.Select(name => SyntaxFactory.Argument(
+                        SyntaxFactory.IdentifierName(name))));
+            return SyntaxFactory.ArgumentList(
+                Token(SyntaxKind.OpenParenToken),
+                arguments,
+                Token(SyntaxKind.CloseParenToken)
+                );
         }
+
+        internal static ArrowExpressionClauseSyntax MethodPointer(
+            string identifier,
+            ParameterListSyntax parameters
+            ) =>
+            SyntaxFactory.ArrowExpressionClause(
+                SyntaxFactory.InvocationExpression(
+                    SyntaxFactory.IdentifierName(identifier),
+                    FromParams(parameters)));
     }
 }
--- test/ProcessorTest.cs
+++ test/ProcessorTest.cs
@@ -1,3 +1,4 @@
+using extern_nll_gen;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
@@ -62,5 +63,51 @@ namespace test
             Assert.Single(methodsBefore);
             Assert.Empty(methodsAfter);
         }
+
+        [Fact]
+        public void ShouldAddLoadFunctionMethod()
+        {
+            var source = @"
+                public static class NativeClass
+                {
+                    [DllImport]
+                    public static extern void myNativeMethod(int param, double[] values);
+                    
+                    public static void OtherMethod() {}
+                }";
+            var processedSource = Process(source);
+            var rootNode = Parse(processedSource);
+            Assert.Contains(rootNode.DescendantNodes(),
+                n =>
+                {
+                    var method = n as MethodDeclarationSyntax;
+                    if (method == null) return false;
+                    if (method.Identifier.Text == Processor.LoadFunctionName) return true;
+                    return false;
+                });
+        }
+
+        [Fact]
+        public void ShouldTransformExternMethods()
+        {
+            var source = @"
+                public static class NativeClass
+                {
+                    [DllImport]
+                    public static extern void Method(int param, double[] values);
+                }";
+            var @delegate = "private delegate void Method_t(int param, double[] values);";
+            var field = "private static Method_t s_Method_t = " +
+                    $"{Processor.LoadFunctionName}<Method_t>(\"Method\");";
+            var method = "public static void Method(int param, double[] values) => s_Method_t(param, values);";
+
+            var processedSource = Process(source);
+
+            Assert.Contains(@delegate, processedSource);
+            Assert.Contains(field, processedSource);
+            Assert.Contains(method, processedSource);
+        }
+
+        // TODO Test DllImport function names
     }
 }
